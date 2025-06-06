9aa679cb8958e859b55e60065853c617efd2587a
Jason Gounder <133807596+HMCJPG@users.noreply.github.com>
2025-06-05 22:51:46 -0700

    Allow dynamic port in connection test

--- tests/connect_test.js
+++ tests/connect_test.js
@@ -2,23 +2,26 @@ const { start } = require('../server');
 const net = require('net');
 const crypto = require('crypto');
 
-const server = start(8765);
+const server = start(0);
 
-const socket = net.createConnection(8765, 'localhost', () => {
-  const key = crypto.randomBytes(16).toString('base64');
-  const req =
-    'GET / HTTP/1.1\r\n' +
-    'Host: localhost\r\n' +
-    'Upgrade: websocket\r\n' +
-    'Connection: Upgrade\r\n' +
-    `Sec-WebSocket-Key: ${key}\r\n` +
-    'Sec-WebSocket-Version: 13\r\n' +
-    '\r\n';
-  socket.write(req);
-});
+server.on('listening', () => {
+  const port = server.address().port;
+  const socket = net.createConnection(port, 'localhost', () => {
+    const key = crypto.randomBytes(16).toString('base64');
+    const req =
+      'GET / HTTP/1.1\r\n' +
+      'Host: localhost\r\n' +
+      'Upgrade: websocket\r\n' +
+      'Connection: Upgrade\r\n' +
+      `Sec-WebSocket-Key: ${key}\r\n` +
+      'Sec-WebSocket-Version: 13\r\n' +
+      '\r\n';
+    socket.write(req);
+  });
 
-socket.once('data', data => {
-  console.log(data.toString().split('\r\n')[0]);
-  socket.destroy();
-  server.close();
+  socket.once('data', data => {
+    console.log(data.toString().split('\r\n')[0]);
+    socket.destroy();
+    server.close();
+  });
 });
